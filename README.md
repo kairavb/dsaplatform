e631ac4df1f531f2e6faa4fe6812758837396b09
kairavb <kairavb8@gmail.com>
2024-10-23 16:14:47 +0530

    testcase improvement

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # dsaplatform
 A platform to practice dsa questions
+
+to check a code corecction,
+it should be
+
+1. compile and run in finite time
+2. have best time and space complexity acc to ans
+3. correct for all the test cases
+
+the type of test cases should be,
+
+1. base cases
+2. edge cases
+3. random cases
+4. duplicates
+5. large inputs
+6. special cases
+
+TODO
+generate better testcases as shown above
+check code finite compile and run as shown above
+a interface to interact and play
--- testCaseCreator.py
+++ testCaseCreator.py
@@ -2,41 +2,62 @@
 # 'ss 0t200 0t500' is testcase constraint, a - array, s - string, n - number, t - to
 # Generates test cases on given code constraints
 
-from random import randrange
+from random import randint, choice
 from sys import argv
 
 
 def type_array(n):
+    testcases = []
+    edgecases = [
+        [n[2]] * n[1],
+        [n[3]] * n[1],
+        [n[2]] * n[0],
+        [n[3]] * n[0],
+        [randint(n[2], n[3]) for _ in range(n[0], n[1]  + 1)],
+        [randint(n[2], n[3]) for _ in range(n[0], n[1]  + 1)]
+    ]
 
-    testarrl = randrange(n[0], n[1] + 1)
-    testarre = []
+    testcases.extend(edgecases)
 
-    for _ in range(testarrl):
-        testarre.append(randrange(n[2], n[3] + 1))
-
-    return testarre
+    while len(testcases) < NUMT:
+        randomcase = [randint(n[2], n[3]) for _ in range(n[0], min(n[1], 100) + 1)]
+        testcases.append(randomcase)
+    
+    return testcases
 
 def type_nums(n):
+    testcases = []
+    edgecases = [n[0], n[1]]
+    testcases.extend(edgecases)
 
-    num = randrange(n[0], n[1] + 1)
+    while len(testcases) < NUMT:
+        randomcase = randint(n[0], min(n[1], 10**7))
+        testcases.append(randomcase)
 
-    return num
+    return testcases
 
 def type_string(n):
-    sn = randrange(n[0], n[1] + 1)
-    s = ''
+    testcases = []
 
-    # 48 - 57 nums
-    # 65 - 90 smallchr
-    # 97 - 122 capchr
-    # 32 - 47 58-64 91-96 123-126 syb
-    for _ in range(sn):
-        s += chr(randrange(32, 126 + 1))
+    stringtype = [chr(i) for i in range(65, 91)]
+    stringtype.extend([chr(i) for i in range(97, 123)])
+    stringtype.extend([chr(i) for i in range(48, 58)])
+
+    edgecases = [
+        ''.join(choice(stringtype) for _ in range(n[0], n[1] + 1)),
+        ''.join(choice(stringtype) for _ in range(n[0], n[1] + 1)),
+        ''.join(choice(stringtype) for _ in range(n[0], n[0] + 1))
+    ]
+    testcases.extend(edgecases)
+
+    while len(testcases) < NUMT:
+        randomcase = ''.join(choice(stringtype) for _ in range(n[0], min(n[1], 100) + 1))
+        testcases.append(randomcase)
     
-    return s
+    return testcases
 
 
-NUMT = 200  # number of testcases per arg
+NUMT = 500  # number of testcases per arg
 
 test = argv[1].split()  # testcase constraint
 
@@ -47,14 +68,17 @@ for i, k in enumerate(test[0]):
 
     if k == 'a':
         with open(f'tests/{argv[1]}.txt', 'a') as file:
-            for _ in range(NUMT):
-                file.write(f'{type_array(n)}\n')
+            tests = type_array(n)
+            for i in tests:
+                file.write(f'{i}\n')
 
     elif k == 's':
         with open(f'tests/{argv[1]}.txt', 'a') as file:
-            for _ in range(NUMT):
-                file.write(f'{type_string(n)}\n')
+            tests = type_string(n)
+            for i in tests:
+                file.write(f'{i}\n')
     else:
         with open(f'tests/{argv[1]}.txt', 'a') as file:
-            for _ in range(NUMT):
-                file.write(f'{type_nums(n)}\n')
+            tests = type_nums(n)
+            for i in tests:
+                file.write(f'{i}\n')
