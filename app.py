09144c32e467774761ee1039404adcd2884ec040
kairavb <kairavb8@gmail.com>
2024-10-29 00:13:26 +0530

    added score system

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from subprocess import run
 
 QRANGE = [1, 3]
 qdata = []
+score = 0
 data = []
 with open(f'data.txt', 'r') as file:
     for line in file:
@@ -16,25 +17,27 @@ app = Flask(__name__)
 @app.route("/", methods=["GET","POST"])
 def index():
     if request.method == "POST":
-        global qdata
+        global qdata, score
         qdata = []
+        score = 0
         qid = int(request.form.get("code"))
 
         if qid == 1:
             n, minutes = 3, 5
         elif qid == 2:
-            n, minutes = 2, 7
+            n, minutes = 5, 7
         elif qid == 3:
-            n, minutes = 3, 10
+            n, minutes = QRANGE[1], 5
         elif qid == 4:
-            n, minutes = 3, 10
+            n, minutes = QRANGE[1], 7
         elif qid == 5:
-            n, minutes = 3, 10
+            n, minutes = QRANGE[1], 10
         else:
-            n, minutes = 3, 10
+            n, minutes = 10, 10
 
         for _ in range(n):
-            qdata.append([data[randint(QRANGE[0], QRANGE[1])][0], minutes])
+            qnum = randint(QRANGE[0], QRANGE[1])
+            qdata.append([data[qnum][0], minutes, data[qnum][4]])
         return redirect("/game")
     else:
         return render_template("index.html")
@@ -42,6 +45,7 @@ def index():
 
 @app.route("/game", methods=["GET","POST"])
 def game():
+    global score
     return_code = 0
     output = ''
     if request.method == "POST":
@@ -53,14 +57,23 @@ def game():
         return_code = result.returncode
         output = result.stdout
 
+        if return_code == 1:
+            score -= 20
+        elif qdata[-1][2] == 0:
+            score += 50
+        elif qdata[-1][2] == 1:
+            score += 70
+        else:
+            score += 100
+
         qdata.pop()
         if len(qdata) == 0:
-            return "done" # results screen
+            return f"your total score is: {score}"
     else:
         if len(qdata) == 0:
             return redirect("/")
 
-    return render_template("game.html", timer=qdata[-1][1], prblm=data[qdata[-1][0]][2], code=return_code, output=output)
+    return render_template("game.html", timer=qdata[-1][1], prblm=data[qdata[-1][0]][2], code=return_code, output=output, score=score)
 
 
 @app.route("/results", methods=["GET","POST"])
